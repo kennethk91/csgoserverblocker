f0086b4f18b1faf69560f32ba5ab8bc0f190b2ee
Kenneth <kkaradas@hotmail.com>
2015-11-05 16:37:52 +0100

    created skeleton

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 900, 460);
+        scene.getStylesheets().add
+                (Main.class.getResource("main.css").toExternalForm());
+        primaryStage.setTitle("CS:GO Server Blocker v1.0");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
